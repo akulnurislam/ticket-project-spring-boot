bb93308cb6f2a90a164687b4cbc2c018e52a3b0a
akul <nurislamimanudin@gmail.com>
2024-03-09 03:47:25 +0700

    user controller

---
+++ src/main/java/com/akul/ticket/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.akul.ticket.controller;
+
+import com.akul.ticket.dto.UserCreateDTO;
+import com.akul.ticket.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/users")
+@Tag(name = "Users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Operation(summary = "Create a User")
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Void> create(@RequestBody @Valid UserCreateDTO dto) {
+        userService.create(dto.mapToUser());
+        return ResponseEntity.created(URI.create("/users")).build();
+    }
+}
---
+++ src/main/java/com/akul/ticket/dto/UserCreateDTO.java
@@ -0,0 +1,22 @@
+package com.akul.ticket.dto;
+
+import com.akul.ticket.model.User;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UserCreateDTO {
+
+    @Schema(defaultValue = "im-people", minLength = 4, maxLength = 50)
+    @NotBlank
+    @Size(min = 4, max = 50)
+    private String username;
+
+    public User mapToUser() {
+        User user = new User();
+        user.setUsername(username);
+        return user;
+    }
+}
