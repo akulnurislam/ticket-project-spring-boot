cc666f8b46390ac3d79e479f4c403c99f54d5804
akul <nurislamimanudin@gmail.com>
2024-03-09 20:30:03 +0700

    update ticket controller

--- src/main/java/com/akul/ticket/controller/TicketController.java
+++ src/main/java/com/akul/ticket/controller/TicketController.java
@@ -6,12 +6,14 @@ import com.akul.ticket.dto.TicketCreateDTO;
 import com.akul.ticket.dto.TicketDTO;
 import com.akul.ticket.service.TicketService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,6 +22,7 @@ import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -39,5 +42,20 @@ public class TicketController {
     public ResponseEntity<TicketDTO> create(@RequestBody @Valid @NonNull TicketCreateDTO dto) {
         var ticket = ticketService.create(dto.mapToModel());
         return ResponseEntity.created(URI.create("/tickets")).body(TicketDTO.mapFromModel(ticket));
+    }
+
+    @Operation(summary = "Get Tickets")
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "ok"),
+            @ApiResponse(responseCode = "400", description = "bad request", content = @Content(schema = @Schema(implementation = ErrorDTO.class))),
+    })
+    public ResponseEntity<List<TicketDTO>> getAll(
+            @Parameter(description = "filter by ticket status [ALL, UPCOMING, CANCELLED, AVAILABLE, ENDED, SOLD_OUT")
+            @Pattern(regexp = "ALL|UPCOMING|CANCELLED|AVAILABLE|ENDED|SOLD_OUT", message = "status must be one of [ALL, UPCOMING, CANCELLED, AVAILABLE, ENDED, SOLD_OUT")
+            @RequestParam(required = false, defaultValue = "AVAILABLE") String status) {
+        var tickets = ticketService.getAll(status);
+        return ResponseEntity.ok(TicketDTO.mapFromModels(tickets));
     }
 }
