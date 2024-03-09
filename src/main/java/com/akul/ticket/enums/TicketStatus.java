35f2b361afd22cc6c67f93ba2dd3d6a8bb779258
akul <nurislamimanudin@gmail.com>
2024-03-09 20:25:25 +0700

    update TicketStatus Enum

--- src/main/java/com/akul/ticket/enums/TicketStatus.java
+++ src/main/java/com/akul/ticket/enums/TicketStatus.java
@@ -1,9 +1,20 @@
 package com.akul.ticket.enums;
 
+import org.springframework.lang.Nullable;
+
 public enum TicketStatus {
     UPCOMING,
     CANCELLED,
     AVAILABLE,
     ENDED,
-    SOLD_OUT,
+    SOLD_OUT;
+
+    @Nullable
+    public static TicketStatus valueOfOrNull(String name) {
+        try {
+            return TicketStatus.valueOf(name);
+        } catch (IllegalArgumentException ignore) {
+            return null;
+        }
+    }
 }
