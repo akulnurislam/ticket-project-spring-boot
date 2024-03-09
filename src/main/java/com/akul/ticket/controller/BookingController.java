e12c0a73bad8f7f59d0e06766dfda215eb933f73
akul <nurislamimanudin@gmail.com>
2024-03-10 00:02:30 +0700

    update booking controller, add security requirement

--- src/main/java/com/akul/ticket/controller/BookingController.java
+++ src/main/java/com/akul/ticket/controller/BookingController.java
@@ -6,13 +6,16 @@ import com.akul.ticket.dto.request.BookingCreateDTO;
 import com.akul.ticket.dto.response.BookingDTO;
 import com.akul.ticket.service.BookingService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +28,7 @@ import java.net.URI;
 @RestController
 @RequestMapping("/bookings")
 @Tag(name = "Bookings")
+@SecurityRequirement(name = "api-key")
 public class BookingController {
 
     private final BookingService bookingService;
@@ -36,8 +40,10 @@ public class BookingController {
     })
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<BookingDTO> create(@RequestBody @Valid @NonNull BookingCreateDTO dto) {
-        var booking = bookingService.create(dto.mapToModel());
+    public ResponseEntity<BookingDTO> create(
+            @Parameter(hidden = true) @RequestHeader(HttpHeaders.AUTHORIZATION) String authorization,
+            @RequestBody @Valid @NonNull BookingCreateDTO dto) {
+        var booking = bookingService.create(dto.mapToModel(authorization));
         return ResponseEntity.created(URI.create("/bookings")).body(BookingDTO.mapFromModel(booking));
     }
 }
