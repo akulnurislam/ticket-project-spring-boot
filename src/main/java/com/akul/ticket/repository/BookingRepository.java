7190dcdd0e96018cbe3881310788805f53cdf1b3
akul <nurislamimanudin@gmail.com>
2024-03-09 21:51:18 +0700

    booking repository

---
+++ src/main/java/com/akul/ticket/repository/BookingRepository.java
@@ -0,0 +1,9 @@
+package com.akul.ticket.repository;
+
+import com.akul.ticket.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BookingRepository extends JpaRepository<Booking, UUID> {
+}
