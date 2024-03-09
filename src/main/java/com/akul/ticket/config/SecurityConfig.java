afcced16e800f418b905827b1e918f9ba28215bc
akul <nurislamimanudin@gmail.com>
2024-03-09 21:53:03 +0700

    update security-config

--- src/main/java/com/akul/ticket/config/SecurityConfig.java
+++ src/main/java/com/akul/ticket/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.akul.ticket.config;
 
+import com.akul.ticket.service.UserService;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
 @SecurityScheme(
         name = "api-key",
         type = SecuritySchemeType.APIKEY,
@@ -13,9 +17,23 @@ import org.springframework.context.annotation.Configuration;
         paramName = "Authorization",
         description = "please put username"
 )
-// @EnableWebSecurity
-public class SecurityConfig {
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig implements WebMvcConfigurer {
     /*
-      Implement web security
+      FOR THE RECORD!
+      this is not the best way for request security
+      it's just simple example for test simplicity
+
+      if you want  a proper request security (jwt, api-key, oauth)
+      then use spring-boot-starter-security
+      such as @EnableWebSecurity
      */
+
+    private final UserService userService;
+
+    @Override
+    public void addInterceptors(@NonNull InterceptorRegistry registry) {
+        registry.addInterceptor(new ApiKeyInterceptor(userService));
+    }
 }
