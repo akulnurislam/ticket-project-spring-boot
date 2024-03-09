3a3127ec421ad36545606bbcf0cb06a5da3dd3b6
akul <nurislamimanudin@gmail.com>
2024-03-09 17:54:13 +0700

    update date-util

--- src/main/java/com/akul/ticket/util/DateUtil.java
+++ src/main/java/com/akul/ticket/util/DateUtil.java
@@ -1,5 +1,8 @@
 package com.akul.ticket.util;
 
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+
 import java.sql.Time;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -9,12 +12,14 @@ import java.time.format.DateTimeFormatter;
 
 public class DateUtil {
 
+    @NonNull
     public static String timestamp() {
         OffsetDateTime timestamp = OffsetDateTime.now(ZoneOffset.UTC);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
         return timestamp.format(formatter);
     }
 
+    @Nullable
     public static Time parseTime(String timeString) {
         SimpleDateFormat sdf = new SimpleDateFormat("hh:mm a");
         try {
@@ -23,5 +28,11 @@ public class DateUtil {
         } catch (ParseException e) {
             return null;
         }
+    }
+
+    @NonNull
+    public static String formatTime(Time time) {
+        SimpleDateFormat sdf = new SimpleDateFormat("hh:mm a");
+        return sdf.format(time);
     }
 }
